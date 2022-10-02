8ae8054b0af581c55330a81e30db6f2ad238e15b
FlorMerino <florenciamerino012@gmail.com>
2022-10-01 21:22:19 -0300

    forceFalse

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 //const port = process.env.PORT_HEROKU ;
 //puesto local 3001
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('%s listening at',process.env.PORT); // eslint-disable-line no-console
   });
